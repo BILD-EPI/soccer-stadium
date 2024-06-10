4ed330037257f46a48dedbed9c4f2e203384ee7c
janine <janine@upday.com>
2024-06-10 17:05:24 +0200

    fix: reset doesnt remove saved postions

--- js/drag.js
+++ js/drag.js
@@ -1,3 +1,51 @@
+// document.querySelectorAll(".button").forEach(function(el){
+//   dragElement(el);
+// });
+
+
+
+
+// function dragElement(elmnt) {
+//     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+//       elmnt.onmousedown  = dragMouseDown;
+//       elmnt.ontouchstart  = dragMouseDown;
+
+//     function dragMouseDown(e) {
+//       e = e || window.event;
+//       e.preventDefault();
+//       // get the mouse cursor position at startup:
+//       pos3 = e.clientX;
+//       pos4 = e.clientY;
+//       document.onmouseup = closeDragElement;
+//       document.addEventListener("touchend", closeDragElement, {passive: false});
+//       // document.ontouchend = closeDragElement;
+//       // call a function whenever the cursor moves:
+//       document.onmousemove = elementDrag;
+//       document.addEventListener('touchmove', elementDrag, {passive: false});
+//       // document.ontouchmove  = elementDrag;
+//     }
+
+//     function elementDrag(e) {
+//       e = e || window.event;
+//       e.preventDefault();
+//       // calculate the new cursor position:
+//       pos1 = pos3 - e.clientX;
+//       pos2 = pos4 - e.clientY;
+//       pos3 = e.clientX;
+//       pos4 = e.clientY;
+//       // set the element's new position:
+//       elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+//       elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+//     }
+
+//     function closeDragElement() {
+//       /* stop moving when mouse button is released:*/
+//       document.onmouseup = null;
+//       document.onmousemove = null;
+//     }
+//   }
+// target elements with the "draggable" class
 document.addEventListener('DOMContentLoaded', function() {
   // Initialize interact.js on elements with the "button" class
   interact('.button').draggable({
@@ -77,11 +125,7 @@ document.addEventListener('DOMContentLoaded', function() {
       button.setAttribute('data-x', 0);
       button.setAttribute('data-y', 0);
 
-      // Clear the saved positions in localStorage
-      for (var i = 1; i <= 3; i++) {
-        localStorage.removeItem(id + '-position-' + i + '-x');
-        localStorage.removeItem(id + '-position-' + i + '-y');
-      }
+      // Do not clear the saved positions in localStorage
     });
   });
 
