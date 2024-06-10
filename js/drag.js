5ab559e3cf48ca4be3eef17a3948a24a5e7ddf2b
janine <janine@upday.com>
2024-06-10 12:29:14 +0200

    added files

---
+++ js/drag.js
@@ -0,0 +1,97 @@
+// document.querySelectorAll(".button").forEach(function(el){
+//   dragElement(el);
+// });
+
+
+
+
+// function dragElement(elmnt) {
+//     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+//       elmnt.onmousedown  = dragMouseDown;
+//       elmnt.ontouchstart  = dragMouseDown;
+
+//     function dragMouseDown(e) {
+//       e = e || window.event;
+//       e.preventDefault();
+//       // get the mouse cursor position at startup:
+//       pos3 = e.clientX;
+//       pos4 = e.clientY;
+//       document.onmouseup = closeDragElement;
+//       document.addEventListener("touchend", closeDragElement, {passive: false});
+//       // document.ontouchend = closeDragElement;
+//       // call a function whenever the cursor moves:
+//       document.onmousemove = elementDrag;
+//       document.addEventListener('touchmove', elementDrag, {passive: false});
+//       // document.ontouchmove  = elementDrag;
+//     }
+
+//     function elementDrag(e) {
+//       e = e || window.event;
+//       e.preventDefault();
+//       // calculate the new cursor position:
+//       pos1 = pos3 - e.clientX;
+//       pos2 = pos4 - e.clientY;
+//       pos3 = e.clientX;
+//       pos4 = e.clientY;
+//       // set the element's new position:
+//       elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+//       elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+//     }
+
+//     function closeDragElement() {
+//       /* stop moving when mouse button is released:*/
+//       document.onmouseup = null;
+//       document.onmousemove = null;
+//     }
+//   }
+// target elements with the "draggable" class
+interact('.button')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'none',
+        endOnly: true
+      })
+    ],
+    // enable autoScroll
+    autoScroll: true,
+
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener,
+
+      // call this function on every dragend event
+      end (event) {
+        var textEl = event.target.querySelector('p')
+
+        textEl && (textEl.textContent =
+          'moved a distance of ' +
+          (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px')
+      }
+    }
+  })
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener
